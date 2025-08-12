43a044ff89a36859de9141f5886dffe54991099b
NitinYdv7668 <nitinyadav1477@gmail.com>
2025-08-12 11:44:50 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+const music = document.getElementById("bg-music");
+music.volume = 0.3; 
+
+const button = document.getElementById("yes-button");
+button.addEventListener("click", () => {
+    alert("You made my day! 💖");
+    alert("Welcome To my life! 💖🫂")
+});
+
+const button1 = document.getElementById("no-button");
+button1.addEventListener("click", () => {
+    alert("You are lose good Person in your Life! 😊")
+});   
